0e2f06b3a508dff4d775ad90db02b2b4ed611231
rndtrash <6745157+rndtrash@users.noreply.github.com>
2022-02-17 01:06:06 +0300

    Add license

--- src/main/java/rndtrash/papermc/welcome/WelcomePlugin.java
+++ src/main/java/rndtrash/papermc/welcome/WelcomePlugin.java
@@ -3,6 +3,12 @@ package rndtrash.papermc.welcome;
 import io.papermc.lib.PaperLib;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+/**
+ * Created by Ivan Kuzmenko on 17.02.2022.
+ *
+ * @author Copyright (c) 2022 Ivan Kuzmenko.
+ */
 public class WelcomePlugin extends JavaPlugin {
 
   @Override
