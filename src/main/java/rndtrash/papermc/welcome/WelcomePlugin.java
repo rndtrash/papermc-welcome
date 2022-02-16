2554529f2cd339bb7c81b1c1e8a0c14d42218446
rndtrash <6745157+rndtrash@users.noreply.github.com>
2022-02-17 01:39:53 +0300

    Actually implemented the MOTD

--- src/main/java/rndtrash/papermc/welcome/WelcomePlugin.java
+++ src/main/java/rndtrash/papermc/welcome/WelcomePlugin.java
@@ -2,6 +2,7 @@ package rndtrash.papermc.welcome;
 
 import io.papermc.lib.PaperLib;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
 
 
 /**
@@ -9,12 +10,53 @@ import org.bukkit.plugin.java.JavaPlugin;
  *
  * @author Copyright (c) 2022 Ivan Kuzmenko.
  */
-public class WelcomePlugin extends JavaPlugin {
+public class WelcomePlugin extends JavaPlugin
+{
+	protected FileConfiguration config;
+	
+	protected string MOTD;
+	protected string NewbieLocal;
+	protected string NewbieGlobal;
+	
+	@Override
+	public void onEnable()
+	{
+		PaperLib.suggestPaper(this);
 
-  @Override
-  public void onEnable() {
-    PaperLib.suggestPaper(this);
-
-    saveDefaultConfig();
-  }
+		saveDefaultConfig();
+		config = getConfig();
+		
+		MOTD = config.getString("motd", "");
+		NewbieLocal = config.getString("newbie.local", "");
+		NewbieGlobal = config.getString("newbie.global", "");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		if (command.getName().equalsIgnoreCase("motd"))
+			SayMOTD(sender);
+	}
+	
+	@Override
+	public void onPlayerJoin(PlayerJoinEvent event)
+	{
+		Player player = event.getPlayer();
+		if (player == null)
+			return;
+		
+		SayMOTD(p);
+		
+		OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(player.getName());
+		if (offlinePlayer.hasPlayedBefore())
+		{
+			Bukkit.broadcastMessage(NewbieGlobal.replace("{u}", player.getName());
+			player.chat(NewbieLocal.replace("{u}", player.getName());
+		}
+	}
+	
+	protected void SayMOTD(CommandSender p)
+	{
+		p.sendMessage(MOTD);
+	}
 }
---
+++ src/test/java/rndtrash/papermc/welcome/ExamplePluginTest.java
@@ -0,0 +1,16 @@
+package rndtrash.papermc.welcome;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Ivan Kuzmenko on 17.02.2022.
+ *
+ * @author Copyright (c) 2022 Ivan Kuzmenko.
+ */
+public class WelcomePluginTest {
+
+  @Test
+  public void onEnable() {
+
+  }
+}
